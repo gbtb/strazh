440565133e74d51a5f590efff875d6a6188f402b
gbtb <goodbetterthebeast3@gmail.com>
2021-10-17 12:41:42 +1000

    init flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+      {
+        devShell.x86_64-linux = pkgs.mkShell {
+
+          buildInputs = with pkgs; [
+            neo4j
+            neo4j-desktop
+            dotnet-sdk_3
+          ];
+        };
+      };
+}
